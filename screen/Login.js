0137f472e3695300e8bd396cb742eb5c9bf32eeb
ss4sitorn <pmss4sitorn@gmail.com>
2024-05-08 16:04:24 +0700

    internet permission android

--- screen/Login.js
+++ screen/Login.js
@@ -53,7 +53,7 @@ const Login = ({ navigation }) => {
 //   container: {
 //     flex: 1,
 //     paddingHorizontal: 20,
-//     paddingTop: 20, 
+//     paddingTop: 20,
 //     backgroundColor: COLORS.cream,
 //   },
 //   headingContainer: {
