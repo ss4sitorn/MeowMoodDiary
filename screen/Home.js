ec12be88b81c6078e3e87b73c7e32f11c10d5208
Taywin Tomthisang <B6501730@g.sut.ac.th>
2024-05-08 20:58:41 +0700

    เพิ่มเงาเอาเท่

--- screen/Home.js
+++ screen/Home.js
@@ -1,11 +1,11 @@
-import { View, Text, TouchableOpacity, StyleSheet, Image } from 'react-native'
-import { Calendar } from 'react-native-calendars';
-import React from 'react'
+import { View, Text, TouchableOpacity, StyleSheet, Image } from "react-native";
+import { Calendar } from "react-native-calendars";
+import React from "react";
 
 const Home = () => {
-    const emotionPath = '../assets/Emotion/e01.png';
-    const date = '2022-01-01';
-    const message = 'This is a message from Firebase';
+  const emotionPath = "../assets/Emotion/e01.png";
+  const date = "2022-01-01";
+  const message = "This is a message from Firebase";
 
   return (
     <View style={styles.container}>
@@ -27,52 +27,75 @@ const Home = () => {
         </TouchableOpacity>
       </View>
     </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#FFF8F8',
+    backgroundColor: "#FFF8F8",
     padding: 20,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
   },
   titleContainer: {
-    alignSelf: 'flex-start',
+    alignSelf: "flex-start",
   },
   title: {
     fontSize: 30,
-    color: '#80B7A2',
+    color: "#80B7A2",
   },
   buttonContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    width: '100%',
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "100%",
     marginTop: 20,
-  },  
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+  },
   button: {
-    backgroundColor: '#FFFFFF',
+    backgroundColor: "#FFFFFF",
     padding: 10,
     borderRadius: 5,
     borderWidth: 1,
-    borderColor: '#800080',
-    width: '45%', // เปลี่ยนเป็น 45%
-    alignItems: 'center',
-    justifyContent: 'center', // เพิ่ม
+    borderColor: "#800080",
+    width: "45%", // เปลี่ยนเป็น 45%
+    alignItems: "center",
+    justifyContent: "center", // เพิ่ม
     margin: 10,
-  },  
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+  },
   buttonText: {
-    color: '#800080',
+    color: "#800080",
     fontSize: 16,
   },
   messageBox: {
-    backgroundColor: '#FFF',
+    backgroundColor: "#FFF",
     borderRadius: 10, // Rounded corners
     padding: 20,
-    alignItems: 'center', // Center everything
-    justifyContent: 'center', // Center everything
+    alignItems: "center", // Center everything
+    justifyContent: "center", // Center everything
     marginTop: 20, // Add some margin at the top
-    borderBlockColor: '#800080',
+    borderBlockColor: "#800080",
     borderWidth: 1,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
   },
   emoji: {
     width: 80,
@@ -80,12 +103,12 @@ const styles = StyleSheet.create({
   },
   date: {
     fontSize: 20,
-    color: '#000',
+    color: "#000",
   },
   message: {
     fontSize: 16,
-    color: '#000',
+    color: "#000",
   },
 });
 
-export default Home
+export default Home;
