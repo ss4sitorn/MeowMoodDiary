1d7f83d1d99e3f3acd8ff92153e5b96c21a7e5ac
Taywin Tomthisang <B6501730@g.sut.ac.th>
2024-05-09 17:25:50 +0700

    แก้สี ลบเงาจ้า HOME

--- screen/Home.js
+++ screen/Home.js
@@ -1,6 +1,7 @@
 import { View, Text, TouchableOpacity, StyleSheet, Image } from "react-native";
 import { Calendar } from "react-native-calendars";
 import React from "react";
+import { COLORS } from "../constants/colors";
 
 const Home = ({ navigation }) => {
   const emotionPath = "../assets/Emotion/e01.png";
@@ -37,69 +38,46 @@ const Home = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#FFF8F8",
+    backgroundColor: COLORS.cream,
     padding: 20,
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
-    elevation: 5,
   },
   titleContainer: {
     alignSelf: "flex-start",
   },
   title: {
     fontSize: 30,
-    color: "#80B7A2",
+    color: COLORS.lightgreen,
   },
   buttonContainer: {
     flexDirection: "row",
     justifyContent: "space-between",
     width: "100%",
     marginTop: 20,
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
   },
   button: {
-    backgroundColor: "#FFFFFF",
+    backgroundColor: COLORS.white,
     padding: 10,
     borderRadius: 5,
     borderWidth: 1,
-    borderColor: "#800080",
+    borderColor: COLORS.purple,
     width: "45%", // เปลี่ยนเป็น 45%
     alignItems: "center",
     justifyContent: "center", // เพิ่ม
     margin: 10,
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
   },
   buttonText: {
-    color: "#800080",
+    color: COLORS.purple,
     fontSize: 16,
   },
   messageBox: {
-    backgroundColor: "#FFF",
+    backgroundColor: COLORS.white,
     borderRadius: 10, // Rounded corners
     padding: 20,
     alignItems: "center", // Center everything
     justifyContent: "center", // Center everything
     marginTop: 20, // Add some margin at the top
-    borderBlockColor: "#800080",
+    borderBlockColor: COLORS.purple,
     borderWidth: 1,
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
   },
   emoji: {
     width: 80,
